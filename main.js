61bb5cc6705836f5ee8d36e446d36572bf05c078
UnicornCodeQueen <75204601+UnicornCodeQueen@users.noreply.github.com>
2021-01-12 12:34:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
-    canvas.createCanvas(300, 300);
-    canvas.center();
+    video.createCapture(VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw() {}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#bf40bf');
+
+    document.getElementById("text_size").innerHTML = "Size of the text will be = " + difference + "px";
+    fill('#00FFFF');
+    stroke('#00FFFF');
+    text(Sridevi, noseX, noseY);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
